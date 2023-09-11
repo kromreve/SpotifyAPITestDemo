95d8123f8e8396f1f4f3832a0ebd5056806c5271
Vaughn Kromrey <kromreve9595@uwec.edu>
2023-09-11 16:04:00 -0500

    bun server addition

--- client/js/login.js
+++ client/js/login.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function () {
     // Redirect to the Spotify authorization URL
     // Replace CLIENT_ID and REDIRECT_URI with your actual values
     const CLIENT_ID = 'b68cdab7b33d4656a3e514c08a3598b5';
-    const REDIRECT_URI = 'http://127.0.0.1:5500/SpotifyAPITestDemo/index.html';
+    const REDIRECT_URI = 'http://localhost:3000/index';
 
     const authUrl = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code&scope=user-read-private%20user-read-email`;
     window.location.href = authUrl;
--- server/server.js
+++ server/server.js
@@ -5,16 +5,16 @@ import { join } from "path";
 
 const server = createServer(async (req, res) => {
   if (req.url === "/login") {
-    const file = await readFile(join(__dirname, "login.html"), "utf8");
+    const file = await readFile(join(__dirname, "../client/login.html"), "utf8");
     res.setHeader("Content-Type", "text/html");
     res.end(file);
   } else if (req.url === "/index") {
-    const file = await readFile(join(__dirname, "index.html"), "utf8");
+    const file = await readFile(join(__dirname, "../client/index.html"), "utf8");
     res.setHeader("Content-Type", "text/html");
     res.end(file);
   } else {
     res.statusCode = 404;
-    res.end("Not found");
+    res.end("Not found bruh");
   }
 });
 
